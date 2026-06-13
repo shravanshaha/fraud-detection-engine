8390f0d75d8e60066c72f2cb7aec5f940c845dab
shravan <shravanshaha18@gmail.com>
2026-06-13 22:12:24 +0530

    Phase 0: Spring Boot setup with Docker MySQL and Swagger configuration

---
+++ src/test/java/com/shravan/frauddetection/FraudDetectionEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shravan.frauddetection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudDetectionEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
